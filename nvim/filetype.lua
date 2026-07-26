dcf9edec58e78f496015f8b7ad175f3ce8fd8a23
Sprootsy <f.bernardi89@gmail.com>
2026-07-26 17:43:13 +0200

    feat(config): added filetype

--- nvim/filetype.lua
+++ nvim/filetype.lua
@@ -1,6 +1,8 @@
 vim.filetype.add({
-  pattern = {
-    ['.*\\.py$'] = 'python',
-    ['docker-compose.yaml'] = 'yaml.docker-compose',
-  },
+	pattern = {
+		[".*\\.py$"] = "python",
+		["docker-compose.yaml"] = "yaml.docker-compose",
+		[".*/.gitconfig.*"] = "gitconfig",
+		[".*/ansible/.*.ya?ml"] = "yaml.ansible",
+	},
 })
